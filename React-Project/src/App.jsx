f0d1ce3665af53c1bcb2d09e15c07b7d0f3e14f3
manojkumar-collab123 <manojberar70@gmail.com>
2026-09-15 15:01:26 +0530

    Clean App routes and remove unused imports

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,31 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-import Home from './components/project/Home'
-import Dashboard from './components/dashboard/Dashboard'
-import Card1 from './view/Card1'
-import ContactForm from './view/Form'
-import Dashborad2 from './components/dashboard/Dashboard2'
-import Table from './view/Table'
-import CardApi from './view/CardApi'
-import SecondFrom from './view/SecondFrom'
-import MapMethod from './view/MapMethod'
-import About from './components/project/About'
-import Services from './components/project/Service'
-import Contact from './components/project/Contact'
-import FromTask2 from './view/FromTask2'
-import Reftab from './view/TAB6'
-import SingleState from './view/SingleState'
-import UseRef1 from './view/UseRef1'
-import UseRef2 from './view/UseRef2'
-import ReducerHook from './view/ReducerHook'
-import TodoReducerHook from './view/TodoReducerHook'
-import { UserProvider } from "./context/UserContext";
-import Contexttry from './view/Contexttry'
-import Navbar from './view/Navbar'
-import ContextDashboard from './view/Dashboar'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserPage from "./view/UserPage";
-import { AuthProvider } from './context/AuthContext';
-import Authentication from "./view/Authentication";
+import { UserProvider } from "./context/UserContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
@@ -33,40 +9,13 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            {/* Previous practice routes are kept here for reference.
-                Uncomment only the route you want to run. */}
-
-            {/* <Route path='/' element={<Dashborad2 />} />
-            <Route path='/cardapi' element={<CardApi />} />
-            <Route path='/tableapi' element={<Table />} />
-            <Route path='/' element={<MapMethod />} />
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/Card1' element={<Card1 />} />
-            <Route path='/' element={<ContactForm />} />
-            <Route path='/' element={<SecondFrom />} />
-            <Route path='/' element={<Home />} />
-            <Route path='/About' element={<About />} />
-            <Route path='/Services' element={<Services />} />
-            <Route path='/Contact' element={<Contact />} />
-            <Route path='/' element={<SingleState />} />
-            <Route path='/' element={<UseRef1 />} />
-            <Route path='/' element={<UseRef2 />} />
-            <Route path='/' element={<ReducerHook />} />
-            <Route path='/' element={<TodoReducerHook />} />
-            <Route path='/' element={<FromTask2 />} />
-            <Route path='/' element={<Reftab />} />
-            <Route path='/1ST' element={<Contexttry />} />
-            <Route path='/2nd' element={<Navbar />} />
-            <Route path='/3rd' element={<ContextDashboard />} />
-            <Route path='/' element={<Authentication />} /> */}
-
-            <Route path='/' element={<UserPage />} />
-            <Route path='/User' element={<UserPage />} />
+            <Route path="/" element={<UserPage />} />
+            <Route path="/User" element={<UserPage />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
     </UserProvider>
-  )
+  );
 }
 
-export default App
+export default App;
